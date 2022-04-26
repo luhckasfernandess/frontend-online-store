03ce243a6dfccd088b61c1b74d6782a2affc9db9
Joao Alberto <joaoalbertosv15@gmail.com>
2022-04-26 16:53:03 -0300

    Adicionano alterações

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Home extends Component {
   render() {
+    const { name } = this.props;
     return (
       <div>
         <p data-testid="home-initial-message">
-          Digite algum termo de pesquisa ou escolha uma categoria.
+          {name}
         </p>
       </div>
     );
   }
 }
+
+Home.propTypes = {
+  name: PropTypes.string.isRequired,
+};
 
 export default Home;
