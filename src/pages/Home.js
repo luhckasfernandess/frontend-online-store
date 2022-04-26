488619b92b81c5fc5eb7025995e52a9ba3f646de
Joao Alberto <joaoalbertosv15@gmail.com>
2022-04-26 18:37:08 -0300

    Req 4

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,22 +2,57 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import Icon from '../img/Cart.svg';
+import { getCategories } from '../services/api';
 
 class Home extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      arrayCategoria: [],
+    };
+  }
+
+  componentDidMount = () => {
+    this.getApiCategoria();
+  }
+
+  getApiCategoria = async () => {
+    const data = await getCategories();
+    console.log(data);
+    this.setState({ arrayCategoria: data });
+  }
+
   render() {
+    const { arrayCategoria } = this.state;
     const { name } = this.props;
     return (
-      <div>
-        <label htmlFor="input-searc">
-          <input type="text" />
-        </label>
-        <Link to="/cart" data-testid="shopping-cart-button">
-          <img src={ Icon } alt="icon" />
-        </Link>
-        <p data-testid="home-initial-message">
-          {name}
-        </p>
-      </div>
+      <main>
+
+        <div>
+          <label htmlFor="input-searc">
+            <input type="text" />
+          </label>
+          <Link to="/cart" data-testid="shopping-cart-button">
+            <img src={ Icon } alt="icon" />
+          </Link>
+          <p data-testid="home-initial-message">
+            {name}
+          </p>
+        </div>
+        <aside>
+          {arrayCategoria.map((item) => (
+            <label
+              htmlFor="input-radio"
+              key={ item.id }
+              data-testid="category"
+            >
+              <input type="radio" />
+              <span>{item.name}</span>
+            </label>
+          ))}
+        </aside>
+      </main>
     );
   }
 }
