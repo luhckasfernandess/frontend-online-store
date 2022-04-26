8ce6a80cd7654e99fa05fa55803e752786d674f8
Joao Alberto <joaoalbertosv15@gmail.com>
2022-04-26 15:56:32 -0300

    Eslint Corrigido

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component() {
   render() {
     return (
       <div>
-        <h1> Projeto</h1>
+        <h1>Projeto</h1>
       </div>
     );
   }
