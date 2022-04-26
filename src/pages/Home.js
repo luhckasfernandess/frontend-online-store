819a0ea73ea243146befb8460f4f89ff3d08be39
Joao Alberto <joaoalbertosv15@gmail.com>
2022-04-26 17:35:04 -0300

    Requisito 3

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Icon } from '@iconify/react';
+import { Link } from 'react-router-dom';
 
 class Home extends Component {
   render() {
     const { name } = this.props;
     return (
       <div>
+        <label htmlFor="input-searc">
+          <input type="text" />
+        </label>
+        <Link to="/cart" data-testid="shopping-cart-button">
+          <Icon icon="ant-design:shopping-cart-outlined" />
+        </Link>
         <p data-testid="home-initial-message">
           {name}
         </p>
