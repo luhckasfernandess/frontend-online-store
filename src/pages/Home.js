6cae24caa43a2f2d1a8afe5ca6a73deee4789060
Joao Alberto <joaoalbertosv15@gmail.com>
2022-04-26 21:55:41 -0300

    Requisito 5

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import Icon from '../img/Cart.svg';
-import { getCategories } from '../services/api';
+import { getCategories, getProductsFromCategoryAndQuery } from '../services/api';
+import Card from '../components/Card';
+import './Home.css';
 
 class Home extends Component {
   constructor() {
@@ -10,6 +12,10 @@ class Home extends Component {
 
     this.state = {
       arrayCategoria: [],
+      arrayProduct: [],
+      product: '',
+      categoria: '',
+      fraseIncial: true,
     };
   }
 
@@ -17,29 +23,59 @@ class Home extends Component {
     this.getApiCategoria();
   }
 
+  handlerChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({ [name]: value });
+  }
+
+  handlerClick = async () => {
+    const { product, categoria } = this.state;
+    console.log(categoria);
+    this.setState({ fraseIncial: true }, async () => {
+      const data = await getProductsFromCategoryAndQuery(categoria, product);
+      this.setState({ fraseIncial: false, arrayProduct: data.results });
+    });
+  }
+
   getApiCategoria = async () => {
     const data = await getCategories();
-    console.log(data);
     this.setState({ arrayCategoria: data });
   }
 
   render() {
-    const { arrayCategoria } = this.state;
+    const { arrayCategoria, fraseIncial, arrayProduct, product } = this.state;
     const { name } = this.props;
+    const frase = <span data-testid="home-initial-message">{name}</span>;
     return (
-      <main>
-
-        <div>
-          <label htmlFor="input-searc">
-            <input type="text" />
-          </label>
-          <Link to="/cart" data-testid="shopping-cart-button">
-            <img src={ Icon } alt="icon" />
-          </Link>
-          <p data-testid="home-initial-message">
-            {name}
-          </p>
-        </div>
+      <main className="main">
+        <form>
+          <div>
+            <label htmlFor="input-search">
+              <input
+                type="text"
+                onChange={ this.handlerChange }
+                name="product"
+                value={ product }
+                data-testid="query-input"
+              />
+            </label>
+            <label htmlFor="input-button">
+              <button
+                type="button"
+                data-testid="query-button"
+                onClick={ this.handlerClick }
+                value={ product }
+                id="input-button"
+              >
+                Search
+              </button>
+            </label>
+            <Link to="/cart" data-testid="shopping-cart-button">
+              <img src={ Icon } alt="icon" />
+            </Link>
+          </div>
+          {fraseIncial ? frase : <Card arrayProduct={ arrayProduct } /> }
+        </form>
         <aside>
           {arrayCategoria.map((item) => (
             <label
@@ -47,7 +83,13 @@ class Home extends Component {
               key={ item.id }
               data-testid="category"
             >
-              <input type="radio" />
+              <input
+                type="checkbox"
+                onChange={ this.handlerClick }
+                value={ item.id }
+                name="categoria"
+                data-testid=""
+              />
               <span>{item.name}</span>
             </label>
           ))}
